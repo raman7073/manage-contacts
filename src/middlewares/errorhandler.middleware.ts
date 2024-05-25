4641e8bc3403e6afa48c389ec6500a9ef3d89878
Raman Mehta <raman.mehta@fiftyfivetech.io>
2024-05-26 04:05:33 +0530

    fix status code for bad request

--- src/middlewares/errorhandler.middleware.ts
+++ src/middlewares/errorhandler.middleware.ts
@@ -8,7 +8,7 @@ function errorMiddleware(
   res: Response,
   next: NextFunction
 ): void {
-  const status = error.status || 500;
+  const status = error.status || 400;
   const message = error.message || "Something went wrong";
   res.status(status).send({
     status,
